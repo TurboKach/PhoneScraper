b2b1a2c2a852ee901a9cef62efc561435152567c
turbokach <incognito@turbokach.me>
2019-07-16 12:06:33 +0300

    Refactoring get_phone_number now returns a list type.

--- phoneScraper.py
+++ phoneScraper.py
@@ -1,15 +1,20 @@
 """This module downloads html page, finds Russian phone numbers
 and returns them as a set of phone numbers in 8KKKNNNNNNN format"""
 
-import urllib.request
 import re
+import urllib.request
 
 regex = r'(?<![.\d])([+]?[7|9|8][ ]?[(]?[4|2|6|5|7|8|9|0]{3}[)]?[ ]?\d{2,3}[- ]?\d{2}[- ]?\d{2})'
+pattern = re.compile(regex)
 
 
 def reformat(string: str):
     """Returns phone number converted to 8KKKNNNNNNN format"""
-    string = string.replace('-', '').replace('(', '').replace(')', '').replace(' ', '').replace('+7', '8')
+    string = string.replace('-', ''). \
+        replace('(', ''). \
+        replace(')', ''). \
+        replace(' ', ''). \
+        replace('+7', '8')
     return string
 
 
@@ -18,10 +23,6 @@ def get_phone_number(url: str):
     req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/6.0'})  # prevents mod_security's HTTP 403
     with urllib.request.urlopen(req) as response:
         page = response.read()
-        pattern = re.compile(regex)
-        phones = re.findall(pattern, str(page))
-        for i in range(len(phones)):
-            phones[i] = reformat(phones[i])
-        phones = set(phones)
+        phones = set(re.findall(pattern, str(page)))
+        phones = list(map(reformat, phones))
     return phones
-
