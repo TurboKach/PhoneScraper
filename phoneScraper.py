9b531a23e2a75f6aa92c1ffba3c78e69a0404ab4
turbokach <incognito@turbokach.me>
2019-07-18 14:35:57 +0300

    Refactoring:  — handling HTTP exceptions  — optimized phone number reformatting

--- phoneScraper.py
+++ phoneScraper.py
@@ -2,27 +2,27 @@
 and returns them as a set of phone numbers in 8KKKNNNNNNN format"""
 
 import re
-import urllib.request
+from urllib import request, error
 
-regex = r'(?<![.\d])([+]?[7|9|8][ ]?[(]?[4|2|6|5|7|8|9|0]{3}[)]?[ ]?\d{2,3}[- ]?\d{2}[- ]?\d{2})'
+regex: str = r'(?<![.\d])([+]?[7|9|8][ ]?[(]?[4|2|6|5|7|8|9|0]{3}[)]?[ ]?\d{2,3}[- ]?\d{2}[- ]?\d{2})'
+reform_regex_bkt: str = r' |\(|\)|-'
+reform_regex_code: str = r'[+]?[7]'
 pattern = re.compile(regex)
+reform_pattern_bkt = re.compile(reform_regex_bkt)
+reform_pattern_code = re.compile(reform_regex_code)
 
 
-def reformat(string: str):
-    """Returns phone number converted to 8KKKNNNNNNN format"""
-    string = string.replace('-', ''). \
-        replace('(', ''). \
-        replace(')', ''). \
-        replace(' ', ''). \
-        replace('+7', '8')
-    return string
-
-
-def get_phone_number(url: str):
+def get_phone_number(url: str) -> set:
     """Returns a set of phone numbers in 8KKKNNNNNNN format"""
-    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/6.0'})  # prevents mod_security's HTTP 403
-    with urllib.request.urlopen(req) as response:
-        page = response.read()
-        phones = set(re.findall(pattern, str(page)))
-        phones = set(map(reformat, phones))
+    req = request.Request(url, headers={'User-Agent': 'Mozilla/6.0'})  # prevents mod_security's HTTP 403
+    try:
+        with request.urlopen(req) as response:
+            print('response: ', response.getcode())
+            page = response.read()
+            phones = pattern.findall(str(page))
+            phones = [re.sub(reform_pattern_bkt, '', s) for s in phones]
+            phones = set([re.sub(reform_regex_code, '8', s) for s in phones])
+    except error.HTTPError as err:
+        if err.code == 404:
+            return {}
     return phones
